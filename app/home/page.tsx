e13ba411d447186fb5efa18e7a2917ebdb70aa6b
Bayurzx <bayurzx@gmail.com>
2023-11-13 20:12:15 +0100

    Added License and completed the front end

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 import { authConfig, loginIsRequiredServer } from "@/lib/auth";
 import { getUserDocListInDb } from "@/lib/astradb";
 import { getServerSession } from "next-auth";
-import { myDocumentation } from "@/lib/fakerData";
+// import { myDocumentation } from "@/lib/fakerData";
 import DocSelect from "./DocSelect";
 
 const wait = (ms: number) => new Promise((rs) => setTimeout(rs, ms));
@@ -12,7 +12,8 @@ export default async function Page() {
   const session = await getServerSession(authConfig);
 
 
-  // const repoList = session?.user?.name ? (await getUserDocListInDb("bayurzxsmtp")) : undefined
+  const repoList = session?.user?.name ? (await getUserDocListInDb("bayurzxsmtp")) : undefined
+  
 
   await wait(1000);
 // consider changing this to cient and using useeffect
@@ -21,7 +22,7 @@ export default async function Page() {
       {/* {session?.user?.image && <img src={session?.user?.image} alt="" />}
       <h3>This is your session.user.email: {session?.user?.email}</h3> */}
 
-      <DocSelect data={myDocumentation} />
+      <DocSelect data={repoList} />
     </>
 
   );
